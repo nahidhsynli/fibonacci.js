8a43b30b5c84bde206ace6f945830b6762ef40b3
nahidhsynli <95674042+nahidhsynli@users.noreply.github.com>
2023-04-14 20:10:12 +0400

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+var number = parseInt(+prompt('Reqem yazin'));
+let number1 = 0, number2 = 1, nextnumber;
+
+console.log("Fibonacci ededleri");
+
+for (let i = 1; i <= nextnumber; i++) {
+    console.log(nextnumber);
+    nextnumber = number1+number2;
+    number1=number2;
+    number2=nextnumber;
+}
+
+
+
